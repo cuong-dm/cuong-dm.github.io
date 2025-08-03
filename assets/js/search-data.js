11e8d83059db716a8a5a7dec492e37efafd83148
cuong-dm <daomanhcuong2001@gmail.com>
2025-08-03 10:25:28 +0000

    Deploying to gh-pages from @ cuong-dm/cuong-dm.github.io@f2a4d0f7ca9bee0a6da256ea3315da31d774d32b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -78,7 +78,52 @@ ninja.data = [{
           section: "News",},{id: "news-1-paper-noisy-data-based-attack-a-new-type-of-untargeted-attack-in-federated-learning-and-its-countermeasures-is-accepted-at-future-generation-computer-systems-q1-journal-if-8-95-h-index-180",
           title: '1 paper Noisy data-based attack: A new type of untargeted attack in Federated...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project.html";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project.html";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project.html";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project.html";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project.html";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project.html";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project.html";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project.html";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project.html";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
